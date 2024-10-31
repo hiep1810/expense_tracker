f7f595a0a0a2f8a59a42898d0b077456ffc816bd
HiepTT <hiep981018@gmail.com>
2024-10-31 22:42:46 +0700

    Update: Add new expense overlay with title input field

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/widgets/expenses_list/expense_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
@@ -30,12 +31,27 @@ class _ExpensesState extends State<Expenses> {
       category: Category.food,
     ),
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          )
+        ]
+      ),
       body: Column(
         children: [
-          const Text('Expense Tracker'),
           Expanded(
             child: ExpenseList(expenses: _registeredExpenses),
           ),
