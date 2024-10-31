8a2d27f8a87febced8dbae46a94c6e232aee7648
HiepTT <hiep981018@gmail.com>
2024-10-31 21:37:55 +0700

    Refactor: Move all expenses_list to expenses_list folder

--- lib/widgets/expenses_list/expense_list.dart
+++ lib/widgets/expenses_list/expense_list.dart
@@ -1,6 +1,6 @@
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
-import 'package:expense_tracker/widgets/expense_item.dart';
+import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 
 class ExpenseList extends StatelessWidget {
   const ExpenseList({super.key, required this.expenses});
