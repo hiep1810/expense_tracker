0c7a84801874785acd8ba33dbdff003d9c828db3
hiep1810 <33901647+hiep1810@users.noreply.github.com>
2024-11-02 23:43:19 +0700

    Refactor: Fix context reference in AlertDialog

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -44,14 +44,14 @@ class _NewExpenseState extends State<NewExpense> {
         _selectedDate == null) {
       showDialog(
         context: context,
-        builder: (ctx) => AlertDialog(
+        builder: (context) => AlertDialog(
             title: const Text('Invalid input'),
             content: const Text(
                 'Please make sure a valid title, amount, date and category was entered.'),
             actions: [
               TextButton(
                   onPressed: () {
-                    Navigator.pop(ctx);
+                    Navigator.pop(context);
                   },
                   child: const Text('Okay'))
             ]),
