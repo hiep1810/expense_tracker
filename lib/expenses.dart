ccb6b080476bc036fae9f663c2b456962c903fd2
hiep1810 <33901647+hiep1810@users.noreply.github.com>
2024-11-03 22:16:26 +0700

    feat: Add Chart widget to display expense statistics in Expenses screen

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/charts/chart.dart';
 import 'package:expense_tracker/widgets/expenses_list/expense_list.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
@@ -93,6 +94,7 @@ class _ExpensesState extends State<Expenses> {
       ]),
       body: Column(
         children: [
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: mainContent,
           ),
