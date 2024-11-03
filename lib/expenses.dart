9b45832b5ccb59c3c3d9877ab028a5c157dcd626
hiep1810 <33901647+hiep1810@users.noreply.github.com>
2024-11-03 18:15:42 +0700

    feat: Add app title 'Expense Tracker' to AppBar

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -83,9 +83,11 @@ class _ExpensesState extends State<Expenses> {
     }
 
     return Scaffold(
-      appBar: AppBar(actions: [
-        IconButton(
-          onPressed: _openAddExpenseOverlay,
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
           icon: const Icon(Icons.add),
         )
       ]),
